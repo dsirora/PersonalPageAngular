a02d6c5112878f00c48bba988a040dd82c97dd52
Sirora <sirora@MacBook-Pro.local>
2020-01-26 15:17:12 -0800

    commit

--- src/app/interests/hobby.service.ts
+++ src/app/interests/hobby.service.ts
@@ -5,20 +5,20 @@ export class HobbyService {
     onDisplay = new EventEmitter<number>();
     private hobbies: Hobby[] = [
         new Hobby("Sports", "https://picsum.photos/id/770/3000/2000",
-        "Cricket is one of the most interesting games to watch and play in its T20 format.",
+        "Cricket is one of the most interesting games to watch and play in its T20 format",
         "I like to watch Wimbledon and other tennis tournaments too. Roger Federer and Sachin are some of my favourite Sport players"
         ),
         new Hobby("Movies", "https://picsum.photos/id/415/5184/3456",
-        "I like watching Adevnuture, Action and Historical Drama films",
-        "Some of my favourite films series include Avengers, Matrix etc."
+        "Movies is not just for entertainment, it can be a stressbuster. I like watching Adevnuture, Action and Historical Drama films",
+        "Some of my favourite films series include Avengers, Matrix etc"
         ),
         new Hobby("Places", "https://picsum.photos/id/323/3831/2554",
-        "I try to travel frequently. Travelling helps me develop a wider world view by exploring different cultures.",
-        "Some of the places which I recently visited include NewYork, Niagara, GrandCanyon.."
+        "I try to travel frequently. Travelling helps me develop a wider world view by exploring different cultures",
+        "Some of the places which I recently visited include NewYork, Niagara, GrandCanyon"
         ),
         new Hobby("Music", "https://picsum.photos/id/334/2304/1536",
-        "I enjoy listening music regularly. Types of Music which I like include Jazz, Rock.",
-        "I like playing guitar & playing piano too."
+        "I enjoy listening music regularly. Types of Music which I like include Classical, Jazz, Rock",
+        "I like playing guitar & playing piano too"
         ),
     ];
 
